5927dd84776540cf1d1a592af88936c72b59dd88
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2023-12-12 23:57:58 +0600

    ui

---
+++ lib/features/presentation/page/news/news_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_app/config/app_colors.dart';
+
+class NewsAPge extends StatelessWidget {
+  const NewsAPge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.backGroundColor,
+      ),
+      body: Column(),
+    );
+  }
+}
